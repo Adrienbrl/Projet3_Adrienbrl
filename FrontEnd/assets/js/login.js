e5e08e2e4be6346b93c5adab1f8fbcae383cae7c
unknown <bruel.adrienpro@gmail.com>
2023-03-09 12:18:56 +0100

    changement du input type submit en button

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,7 +1,35 @@
-const formulaire = document.querySelector("form")
+const formulaire = document.querySelector("form");
+const email = document.getElementById ("mail");
+const password = document.getElementById ("password");
+const errorConnect =document.querySelector (".error-connect");
 
-form.addEventListener("submit", function(e){
-    e.preventDefault()
-    console.log("Bonjour à tous")
-    
-})
+formulaire.addEventListener("submit", async function(event){
+    event.preventDefault()
+
+    const logsApi = "http://localhost:5678/api/users/login";
+    const fetchInit = {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body : JSON.stringify({
+        email: email.value,
+        password: password.value,
+      }),
+    };
+    try {
+      const response = await fetch (logsApi, fetchInit);
+      if (response.ok) {
+        const data = await response.json();
+        sessionStorage.setItem("token", data.token);
+        location.href = "index.html";
+      } if (response.status === 401) {
+          errorConnect.console.log("Mot de passe incorrect");
+      }   else if (response.status === 404) {
+          errorConnect.console.log("Mot de passe et/ou identifiant incorrect");
+      }
+    } catch (error) {
+      console.error("Warning : " + error);
+    }
+});
